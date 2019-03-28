8006740746edd89ac3566109b97ea663b3cfa681
scottluskcis <scottlusk.cis@outlook.com>
2019-03-28 10:39:04 -0400

    add new ASP.NET Core Console Application

---
+++ Log4NetInCoreExample/Log4NetInCoreExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Log4NetInCoreExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
